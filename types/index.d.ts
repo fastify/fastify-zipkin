2dadc6d4f308f327208370428640927f1c44d5ea
uzlopak <aras.abbasi@googlemail.com>
2022-11-23 15:40:29 +0100

    nodenext compatibility

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,13 +1,21 @@
 import { FastifyPluginCallback } from 'fastify';
 import { Recorder, Tracer } from 'zipkin';
 
-interface FastifyZipkinOptions {
-  serviceName: string;
-  httpReporterUrl: string;
-  servicePort?: number;
-  recorder?: Recorder;
-  tracer?: Tracer;
+type FastifyZipkin = FastifyPluginCallback<fastifyZipkin.FastifyZipkinOptions>;
+
+declare namespace fastifyZipkin {
+  export interface FastifyZipkinOptions {
+    serviceName: string;
+    httpReporterUrl: string;
+    servicePort?: number;
+    recorder?: Recorder;
+    tracer?: Tracer;
+  }
+
+  export const fastifyZipkin: FastifyZipkin
+  export { fastifyZipkin as default }
+
 }
 
-declare const fastifyZipkin: FastifyPluginCallback<FastifyZipkinOptions>;
-export default fastifyZipkin;
+declare function fastifyZipkin(...params: Parameters<FastifyZipkin>): ReturnType<FastifyZipkin>
+export = fastifyZipkin
